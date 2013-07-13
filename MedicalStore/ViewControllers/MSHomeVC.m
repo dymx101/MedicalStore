3791cafd17f7752d9009ca5de19b5bfc3d877536
dymx101 <dongyiming@Dongs-MacBook-Pro.local>
2013-07-13 09:56:37 -0700

    add slide view

--- MedicalStore/ViewControllers/MSHomeVC.m
+++ MedicalStore/ViewControllers/MSHomeVC.m
@@ -8,11 +8,18 @@
 
 #import "MSHomeVC.h"
 
-@interface MSHomeVC ()
+#import "KASlideShow.h"
+
+
+
+@interface MSHomeVC () <KASlideShowDelegate>
 
 @end
 
 @implementation MSHomeVC
+{
+    KASlideShow         *_viewSlideShow;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,12 +33,41 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self _initLayout];
+}
+
+-(void)_initLayout
+{
+    _viewSlideShow = [[KASlideShow alloc] initWithFrame:CGRectMake(0, 0, 320, 100)];
+    [self.view addSubview:_viewSlideShow];
+    
+    _viewSlideShow.delegate = self;
+    [_viewSlideShow setDelay:3]; 
+    [_viewSlideShow setTransitionDuration:.5f]; 
+    [_viewSlideShow setTransitionType:KASlideShowTransitionSlide];
+    [_viewSlideShow setImagesContentMode:UIViewContentModeScaleAspectFill]; 
+    [_viewSlideShow addImagesFromResources:@[@"sample_adv1.jpg",@"sample_adv2.jpg",@"sample_adv3.jpg"]];
+    [_viewSlideShow start];
 }
 
 
 - (NSString *)tabImageName
 {
 	return @"tab_home";
+}
+
+
+#pragma mark - KASlideShow delegate
+
+- (void)kaSlideShowDidNext
+{
+    //NSLog(@"kaSlideShowDidNext");
+}
+
+-(void)kaSlideShowDidPrevious
+{
+    //NSLog(@"kaSlideShowDidPrevious");
 }
 
 @end
