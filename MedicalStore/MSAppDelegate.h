01463e3ce3e22e7e57a698a4fd7897025769d4d0
dymx101 <dongyiming@Dongs-MacBook-Pro.local>
2013-07-13 07:10:50 -0700

    Initial Commit

---
+++ MedicalStore/MSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSAppDelegate.h
+//  MedicalStore
+//
+//  Created by Dong Yiming on 7/13/13.
+//  Copyright (c) 2013 Dong Yiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
