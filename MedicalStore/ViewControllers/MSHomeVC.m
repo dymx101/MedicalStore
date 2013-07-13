66c048323eaaaf31a77faebc9c19fe2236573600
dymx101 <dongyiming@Dongs-MacBook-Pro.local>
2013-07-13 09:14:33 -0700

    some change

--- MedicalStore/ViewControllers/MSHomeVC.m
+++ MedicalStore/ViewControllers/MSHomeVC.m
@@ -26,14 +26,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 
@@ -41,12 +33,5 @@
 {
 	return @"tab_home";
 }
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    return YES;
-}
-
-
 
 @end
