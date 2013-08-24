19cc9431626cbd9b1add60b0016efb2cdd40dfff
dymx101 <dongyiming@Dongs-MacBook-Pro.local>
2013-08-24 16:45:29 +0800

    UI

--- MedicalStore/ViewControllers/GGProfileVC.m
+++ MedicalStore/ViewControllers/GGProfileVC.m
@@ -13,6 +13,11 @@
 @interface GGProfileVC ()
 @property (weak, nonatomic) IBOutlet UITextField *tfName;
 @property (weak, nonatomic) IBOutlet UITextField *tfPhone;
+@property (weak, nonatomic) IBOutlet UITextField *tfValidate;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnGetValidCode;
+@property (weak, nonatomic) IBOutlet UIButton *btnValiate;
+
 @property (weak, nonatomic) IBOutlet UIView *viewTip;
 
 @end
@@ -24,7 +29,7 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
 //        self.title = @"个人资料";
-        [self setTitle:@"个人资料"];
+        [self setTitle:@"身份验证"];
     }
     return self;
 }
@@ -36,16 +41,22 @@
     _tfName.text = [GGUserDefault myName];
     _tfPhone.text = [GGUserDefault myPhone];
     
-    UIBarButtonItem *barBtn = [[UIBarButtonItem alloc] initWithTitle:@"保存" style:UIBarButtonItemStyleBordered target:self action:@selector(save:)];
-    self.navigationItem.rightBarButtonItem = barBtn;
+    //UIBarButtonItem *barBtn = [[UIBarButtonItem alloc] initWithTitle:@"保存" style:UIBarButtonItemStyleBordered target:self action:@selector(save:)];
+    //self.navigationItem.rightBarButtonItem = barBtn;
     
     [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(endEditing)]];
+    
+    [self.btnGetValidCode addTarget:self action:@selector(getValidateCodeAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.btnValiate addTarget:self action:@selector(validateAction:) forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)viewDidUnload {
     [self setTfName:nil];
     [self setTfPhone:nil];
     [self setViewTip:nil];
+    [self setTfValidate:nil];
+    [self setBtnGetValidCode:nil];
+    [self setBtnValiate:nil];
     [super viewDidUnload];
 }
 
@@ -62,6 +73,16 @@
     NSArray * profile = [NSArray arrayWithObjects:self.tfName.text,self.tfPhone.text,nil];
     [GGArchive archiveData:profile withFileName:@"profile.plist"];
     [self.navigationController popViewControllerAnimated:YES];
+}
+
+-(IBAction)getValidateCodeAction:(id)sender
+{
+    DLog(@"getValidateCodeAction");
+}
+
+-(IBAction)validateAction:(id)sender
+{
+    DLog(@"validateAction");
 }
 
 @end
